a99ac4071cbde4289ebac77f14eaabcf80b75a60
jsteeleiv <jsteele@ecpi.edu>
2023-01-14 19:58:50 -0500

    Implemented output file

--- ppng.c
+++ ppng.c
@@ -9,8 +9,17 @@
 #include <stdint.h>
 
 #define PNG_SIG_CAP 8
+#define PNG_DAT_CAP (64*1024)
 
-uint8_t png_sig[] = {137, 80, 78, 71, 13, 10, 26, 10};
+const uint8_t png_sig[] = {137, 80, 78, 71, 13, 10, 26, 10};
+
+//would this be a better approach? Look into small helper func
+struct Chunk{
+	uint32_t length;
+	uint32_t type;
+	uint32_t crc;
+	uint8_t data[PNG_DAT_CAP];  //chunk.length determines data size(max:2^31)
+};
 
 void read_buf(FILE *file, void *buf, size_t buf_cap)
 {
@@ -32,20 +41,14 @@ void read_buf(FILE *file, void *buf, size_t buf_cap)
 void print_bytes(uint8_t *buf, size_t buf_cap)
 {
 	for(size_t i=0; i<buf_cap; ++i){
-				printf("%u ", buf[i]);
+		printf("%u ", buf[i]);
 	}
+	printf("\n");
 }
 
 int main(int argc, char **argv)
 {
 	(void) argc;
-
-	struct Chunk{
-		uint32_t length;
-		uint32_t type;
-		uint8_t data[];  //chunk.length determines data size(max:2^31)
-
-	};
 
 	//assert that there was an arg provided; the arg array providesa null terminator, allowing us to step through args until reaching NULL
 	assert(*argv != NULL);
@@ -57,18 +60,31 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 
-	//take next arg as file path
-	char *filepath = *argv++;
+	//take next arg as in_filepath
+	char *in_filepath = *argv++;
 	
-	FILE *input_file = fopen(filepath, "rb");
+	FILE *input_file = fopen(in_filepath, "rb");
 	if (input_file == NULL){
 		fprintf(stderr, "ERROR: could not open file %s: %s\n", 
-				filepath, strerror(errno));
+				in_filepath, strerror(errno));
 		exit(1);
 
 	}
-	printf("Provided file: %s\n", filepath);
+	printf("Provided file: %s\n", in_filepath);
 
+	if(*arg++!=NULL){
+		//take next arg as out_filepath
+		char *out_filepath = *argv++;
+		
+		FILE *output_file = fopen(out_filepath, "wb");
+		if (output_file == NULL){
+			fprintf(stderr, "ERROR: could not open file %s: %s\n", 
+					out_filepath, strerror(errno));
+			exit(1);
+		}
+	}
+
+	printf("Destination file: %s\n", out_filepath);
 	//png signature; first 8 bytes of file ALWAYS contains
 	//the following decimal values: 137 80 78 71 13 10 26 10
 	
@@ -78,10 +94,10 @@ int main(int argc, char **argv)
 	print_bytes(sig, PNG_SIG_CAP);
 
 	if(memcmp(sig, png_sig, PNG_SIG_CAP)!=0){
-		fprintf(stderr, "ERROR: %s does not appear to be a valid PNG image\n", filepath);
+		fprintf(stderr, "ERROR: %s does not appear to be a valid PNG image\n", in_filepath);
 		exit(1);
 	} 
-	printf("%s is a valid PNG image\n", filepath);
+	printf("%s is a valid PNG image\n", in_filepath);
 	fclose(input_file);
 	return 0;
 }
