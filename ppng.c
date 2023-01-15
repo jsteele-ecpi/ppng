a6de120f9d386df8b999f15da463d07a0c4b4e8b
jsteeleiv <jsteele@ecpi.edu>
2023-01-15 00:49:34 -0500

    added MIT license

--- ppng.c
+++ ppng.c
@@ -21,7 +21,7 @@ struct Chunk{
 	uint8_t data[PNG_DAT_CAP];  //chunk.length determines data size(max:2^31)
 };
 
-void read_buf(FILE *file, void *buf, size_t buf_cap)
+void read_bytes(FILE *file, void *buf, size_t buf_cap)
 {
 	size_t n = fread(buf, buf_cap, 1, file);
 	if(n!=1){
@@ -46,12 +46,29 @@ void print_bytes(uint8_t *buf, size_t buf_cap)
 	printf("\n");
 }
 
+void flip_bytes(void *buf_, size_t buf_cap)
+{
+	uint8_t *buf = buf_;
+	for(size_t i = 0; i < buf_cap/2; ++i){
+		uint8_t t = buf[i];
+		buf[i] = buf[buf_cap - i - 1];
+		buf[buf_cap - i - 1] = t;
+	}
+}
+
+void skip_bytes(void *buf_, size_t offset, size_t buf_cap)
+{
+	
+}
+
 int main(int argc, char **argv)
 {
 	(void) argc;
 
 	//assert that there was an arg provided; the arg array providesa null terminator, allowing us to step through args until reaching NULL
 	assert(*argv != NULL);
+
+	//take 1st arg provided, then increment the arg count
 	char *program = *argv++;
 
 	if(*argv == NULL){
@@ -65,39 +82,57 @@ int main(int argc, char **argv)
 	
 	FILE *input_file = fopen(in_filepath, "rb");
 	if (input_file == NULL){
-		fprintf(stderr, "ERROR: could not open file %s: %s\n", 
+		fprintf(stderr, "READ_ERROR: could not open file %s: %s\n", 
 				in_filepath, strerror(errno));
 		exit(1);
 
 	}
 	printf("Provided file: %s\n", in_filepath);
 
-	if(*arg++!=NULL){
+	if(*argv++!=NULL){
 		//take next arg as out_filepath
 		char *out_filepath = *argv++;
 		
 		FILE *output_file = fopen(out_filepath, "wb");
 		if (output_file == NULL){
-			fprintf(stderr, "ERROR: could not open file %s: %s\n", 
+			fprintf(stderr, "WRITE_ERROR: could not open file %s: %s\n", 
 					out_filepath, strerror(errno));
 			exit(1);
 		}
+		printf("Destination file: %s\n", out_filepath);
 	}
 
-	printf("Destination file: %s\n", out_filepath);
 	//png signature; first 8 bytes of file ALWAYS contains
 	//the following decimal values: 137 80 78 71 13 10 26 10
 	
 	//capture first 8 bytes and store as array?
 	uint8_t sig[PNG_SIG_CAP];
-	read_buf(input_file, sig, PNG_SIG_CAP);
+	read_bytes(input_file, sig, PNG_SIG_CAP);
+	printf("PNG Signature:\n");
 	print_bytes(sig, PNG_SIG_CAP);
 
 	if(memcmp(sig, png_sig, PNG_SIG_CAP)!=0){
 		fprintf(stderr, "ERROR: %s does not appear to be a valid PNG image\n", in_filepath);
 		exit(1);
 	} 
-	printf("%s is a valid PNG image\n", in_filepath);
+	uint32_t chunk_len;
+	read_bytes(input_file, &chunk_len, sizeof(chunk_len));
+	flip_bytes(&chunk_len, sizeof(chunk_len));
+	printf("Chunk Length (data): %u\n", chunk_len);
+
+	uint8_t chunk_type[4];
+	read_bytes(input_file, chunk_type, sizeof(chunk_type));
+	printf("Chunk Type : %.*s\n", (int)sizeof(chunk_type), chunk_type); 
+	printf("Chunk Bytes: ");
+	print_bytes(chunk_type, sizeof(chunk_type));
+
+	uint8_t chunk_data[chunk_len];
+	read_bytes(input_file, chunk_type, sizeof(chunk_type));
+	print_bytes(chunk_data, sizeof(chunk_data));
+
+
+
+	//printf("%s is a valid PNG image\n", in_filepath);
 	fclose(input_file);
 	return 0;
 }
