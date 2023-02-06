ea3b517b041cc873c37a8bf8b8e79ea66874b0c6
jsteeleiv <jsteele@ecpi.edu>
2023-02-05 20:59:07 -0500

    added functions to create crc

--- ppng.c
+++ ppng.c
@@ -12,6 +12,56 @@
 #define PNG_SIG_CAP 8
 #define PNG_DAT_CAP (64*1024)
 
+//http://www.libpng.org/pub/png/spec/1.2/PNG-CRCAppendix.html
+//c code to implement a crc (cyclic redundancy check)
+   /* Table of CRCs of all 8-bit messages. */
+   unsigned long crc_table[256];
+   
+   /* Flag: has the table been computed? Initially false. */
+   int crc_table_computed = 0;
+   
+   /* Make the table for a fast CRC. */
+   void make_crc_table(void)
+   {
+     unsigned long c;
+     int n, k;
+     for (n = 0; n < 256; n++) {
+       c = (unsigned long) n;
+       for (k = 0; k < 8; k++) {
+         if (c & 1)
+           c = 0xedb88320L ^ (c >> 1);
+         else
+           c = c >> 1;
+       }
+       crc_table[n] = c;
+     }
+     crc_table_computed = 1;
+   }
+   /* Update a running CRC with the bytes buf[0..len-1]--the CRC
+      should be initialized to all 1's, and the transmitted value
+      is the 1's complement of the final running CRC (see the
+      crc() routine below)). */
+   
+   unsigned long update_crc(unsigned long crc, unsigned char *buf, int len)
+   {
+     unsigned long c = crc;
+     int n;
+   
+     if (!crc_table_computed)
+       make_crc_table();
+     for (n = 0; n < len; n++) {
+       c = crc_table[(c ^ buf[n]) & 0xff] ^ (c >> 8);
+     }
+     return c;
+   }
+   
+   /* Return the CRC of the bytes buf[0..len-1]. */
+   unsigned long crc(unsigned char *buf, int len)
+   {
+     return update_crc(0xffffffffL, buf, len) ^ 0xffffffffL;  //Suffix L indicates a long value (at least 32 bits).
+   }
+
+
 const uint8_t png_sig[] = {137, 80, 78, 71, 13, 10, 26, 10};
 bool quit = false;
 
@@ -97,6 +147,8 @@ void inject_chunk(FILE *file, void *buf, size_t buf_cap, uint8_t chunk_type[4])
 
 }
 
+//TODO: create function to handle usage message
+void usage();
 
 int main(int argc, char **argv)
 {
